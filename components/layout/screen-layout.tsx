6a09fcac1bd752da2e14cc5044b695e047c13a9b
Emmanuel <emmsom506@gmail.com>
2026-02-18 16:59:32 +0000

    Chore screen layout

--- components/layout/screen-layout.tsx
+++ components/layout/screen-layout.tsx
@@ -1,28 +1,72 @@
+import React, {ReactNode} from "react";
 import {SafeAreaView} from "react-native-safe-area-context";
-import {Keyboard, TouchableWithoutFeedback, View, ViewStyle} from "react-native";
+import {Navbar} from "@/components/layout/nav-bar";
+import {Animated, View} from "react-native";
+import ScrollView = Animated.ScrollView;
 
-interface ScreenLayoutProps {
+type ScreenLayoutProps = {
+  children: ReactNode;
   screen: string;
-  children: React.ReactNode;
-  contentContainerStyle?: ViewStyle;
-}
 
-export default function ScreenLayout(
+  showNavbar?: boolean;
+  navbarTitle?: string;
+  showBack?: boolean;
+  onBackPress?: () => void;
+  navbarLeftContent?: ReactNode;
+  navbarRightContent?: ReactNode;
+  navbarVariant?: "default" | "transparent";
+
+  scrollable?: boolean;
+  className?: string;
+};
+
+const ScreenLayout = (
   {
-    screen,
     children,
-    contentContainerStyle,
-  }: ScreenLayoutProps) {
-  return (
-   <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
-     <SafeAreaView edges={['top']} style={{flex: 1}} id={screen}>
-       <View
-         style={contentContainerStyle || {paddingHorizontal: 16, flex: 1}}
-         className={"bg-background text-text"}
-       >
-         {children}
-       </View>
-     </SafeAreaView>
-   </TouchableWithoutFeedback>
+    screen,
+    showNavbar = true,
+    navbarTitle,
+    showBack = false,
+    onBackPress,
+    navbarLeftContent,
+    navbarRightContent,
+    navbarVariant = "default",
+    scrollable = true,
+    className = "",
+  }: ScreenLayoutProps) => {
+
+  const content = scrollable ? (
+    <ScrollView
+      contentContainerClassName={`${className}`}
+      showsVerticalScrollIndicator={false}
+    >
+      {children}
+    </ScrollView>
+  ) : (
+    <View className={`${className}`}>
+      {children}
+    </View>
   );
-}
+
+  return (
+    <SafeAreaView className="flex-1" edges={["top"]}>
+      <View className="flex-1" id={screen}>
+        {showNavbar && (
+          <Navbar
+            title={navbarTitle}
+            showBack={showBack}
+            onBackPress={onBackPress}
+            leftContent={navbarLeftContent}
+            rightContent={navbarRightContent}
+            variant={navbarVariant}
+          />
+        )}
+       <View className="p-4 flex-1 bg-background">
+         {content}
+       </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ScreenLayout;
