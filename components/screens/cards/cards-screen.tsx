718ea4f25e1125378aa054dfe800f77a72b1df3a
Emmanuel <emmsom506@gmail.com>
2026-02-28 19:54:51 +0000

    Add cards screen

---
+++ components/screens/cards/cards-screen.tsx
@@ -0,0 +1,15 @@
+import ScreenLayout from "@/components/layout/screen-layout";
+
+const CardsScreen = () => {
+  return(
+    <ScreenLayout
+      screen={"cards"}
+      navbarTitle={"Cards"}
+    >
+      <div>
+        Cards screen
+      </div>
+    </ScreenLayout>
+  )
+}
+export default CardsScreen;
