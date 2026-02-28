21f7830ba42027ea66743195026ec8b776b7d3c5
Emmanuel <emmsom506@gmail.com>
2026-02-28 19:54:04 +0000

    Add settings tab

---
+++ components/screens/setttings/settings-screen.tsx
@@ -0,0 +1,15 @@
+import ScreenLayout from "@/components/layout/screen-layout";
+
+const SettingsScreen = () => {
+  return(
+    <ScreenLayout
+      screen={"settings"}
+      navbarTitle={"Settings"}
+    >
+      <div>
+        Settings page
+      </div>
+    </ScreenLayout>
+  )
+}
+export default SettingsScreen;
