7880c36b46a7179a65212360bb4a4f1e31b21f21
Emmanuel <emmsom506@gmail.com>
2026-03-21 12:02:05 +0000

    Fix app button issue

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,15 +1,15 @@
-import "react-native-url-polyfill/auto";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Platform } from "react-native";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage,
+    storage: Platform.OS === "web" ? undefined : AsyncStorage,
     autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
+    persistSession: Platform.OS !== "web",
+    detectSessionInUrl: Platform.OS === "web",
   },
 });
